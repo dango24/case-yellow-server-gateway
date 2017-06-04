669b8e6a4f1bb738a6905084f1e8b224424eb399
dango <dango@checkpoint.com>
2017-06-04 17:17:06 +0300

    Initial commit

---
+++ src/main/java/com/icarusrises/caseyellow/server/gateway/CaseYellowServerGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.icarusrises.caseyellow.server.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseYellowServerGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseYellowServerGatewayApplication.class, args);
+	}
+}
---
+++ src/main/java/com/icarusrises/caseyellow/server/gateway/speedtestwebsite/controllers/SpeedTestWebSiteController.java
@@ -0,0 +1,7 @@
+package com.icarusrises.caseyellow.server.gateway.speedtestwebsite.controllers;
+
+/**
+ * Created by dango on 6/4/17.
+ */
+public class SpeedTestWebSiteController {
+}
