d9c847ea89066e269ca63e8b4b877cc0486068cb
dango <dango@checkpoint.com>
2017-10-03 21:38:23 +0300

    add multi file support

--- src/test/java/com/caseyellow/server/central/config/DataSourceConfig.java
+++
@@ -1,9 +0,0 @@
-package com.caseyellow.server.central.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("com.caseyellow.server.central.data.source")
-public class DataSourceConfig {
-}
--- src/test/java/com/caseyellow/server/central/data/source/FakeDataSource.java
+++
@@ -1,5 +0,0 @@
-package com.caseyellow.server.central.data.source;
-
-public interface FakeDataSource {
-    String getConnection();
-}
