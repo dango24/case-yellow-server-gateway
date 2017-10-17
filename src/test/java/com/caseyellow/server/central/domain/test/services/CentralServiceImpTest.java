fd2d0911cccc90d896878395f3bc799cb94a31bf
Dan <dangolan24@gmail.com>
2017-10-17 09:52:06 +0300

    add dummy_tests

---
+++ src/test/java/com/caseyellow/server/central/domain/test/services/CentralServiceImpTest.java
@@ -0,0 +1,49 @@
+package com.caseyellow.server.central.domain.test.services;
+
+import com.caseyellow.server.central.CaseYellowCentral;
+import com.caseyellow.server.central.domain.test.model.Test;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = CaseYellowCentral.class)
+@ActiveProfiles("dev")
+public class CentralServiceImpTest {
+
+    private static final String DUMMY_TEST_LOCATION = "/dummy_tests.json";
+
+    @org.junit.Test
+    public void saveTest() throws Exception {
+        Path testPath = Paths.get(CentralServiceImpTest.class.getResource(DUMMY_TEST_LOCATION).toURI());
+        TestWrapper testWrapper = new ObjectMapper().readValue(testPath.toFile(), TestWrapper.class);
+
+        assertNotNull(testWrapper.getTests());
+    }
+
+    public static class TestWrapper {
+
+        private List<Test> tests;
+
+        public TestWrapper() {
+        }
+
+        public List<Test> getTests() {
+            return tests;
+        }
+
+        public void setTests(List<Test> tests) {
+            this.tests = tests;
+        }
+    }
+
+}
