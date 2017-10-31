23dac2077d16b6cdcb9c125385d5850a4bca240a
dango <dango@checkpoint.com>
2017-10-31 17:49:02 +0200

    Add speed test random mechanism

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 FROM nathonfowlie/centos-jre:1.8.0_60
 WORKDIR /app
 COPY /target/case-yellow-central-0.0.1-SNAPSHOT.jar /app
+RUN ["apt-get", "update"]
+RUN ["apt-get", "install", "-y", "vim"]
 CMD java -jar case-yellow-central-0.0.1-SNAPSHOT.jar
--- src/main/java/com/caseyellow/server/central/domain/webSite/services/SpeedTestWebSiteServiceImpl.java
+++ src/main/java/com/caseyellow/server/central/domain/webSite/services/SpeedTestWebSiteServiceImpl.java
@@ -1,9 +1,14 @@
 package com.caseyellow.server.central.domain.webSite.services;
 
+import com.caseyellow.server.central.common.UrlMapper;
 import com.caseyellow.server.central.domain.webSite.model.SpeedTestMetaData;
 import com.caseyellow.server.central.persistence.website.repository.SpeedTestWebSiteCounterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 /**
  * Created by dango on 9/19/17.
@@ -11,17 +16,20 @@ import org.springframework.stereotype.Service;
 @Service
 public class SpeedTestWebSiteServiceImpl implements SpeedTestWebSiteService {
 
+    private UrlMapper urlMapper;
     private SpeedTestWebSiteFactory speedTestWebSiteFactory;
-    private SpeedTestWebSiteCounterRepository speedTestWebSiteCounterRepository;
 
     @Autowired
-    public SpeedTestWebSiteServiceImpl(SpeedTestWebSiteCounterRepository speedTestWebSiteCounterRepository, SpeedTestWebSiteFactory speedTestWebSiteFactory) {
-        this.speedTestWebSiteCounterRepository = speedTestWebSiteCounterRepository;
+    public SpeedTestWebSiteServiceImpl(UrlMapper urlMapper, SpeedTestWebSiteFactory speedTestWebSiteFactory) {
+        this.urlMapper = urlMapper;
         this.speedTestWebSiteFactory = speedTestWebSiteFactory;
     }
 
     @Override
     public SpeedTestMetaData getNextSpeedTestWebSite() {
-        return speedTestWebSiteFactory.getSpeedTestWebSiteFromIdentifier(speedTestWebSiteCounterRepository.findMinIdentifier());
+        List<String> speedTestIdentifiers = new ArrayList<>(urlMapper.getSpeedTestIdentifiers());
+        int random = new Random().nextInt(speedTestIdentifiers.size());
+
+        return speedTestWebSiteFactory.getSpeedTestWebSiteFromIdentifier(speedTestIdentifiers.get(random));
     }
 }
