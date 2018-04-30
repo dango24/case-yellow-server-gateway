a62dd29e47276d227dcae475a434d1869e4501fc
Dan <dangolan24@gmail.com>
2018-04-30 23:07:25 +0300

    clean ups

--- src/main/java/com/caseyellow/server/central/domain/webSite/model/SpeedTestWebSite.java
+++ src/main/java/com/caseyellow/server/central/domain/webSite/model/SpeedTestWebSite.java
@@ -1,9 +1,16 @@
 package com.caseyellow.server.central.domain.webSite.model;
 
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * Created by Dan on 12/10/2016.
  */
+@Data
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class SpeedTestWebSite {
 
     private boolean succeed;
@@ -14,9 +21,6 @@ public class SpeedTestWebSite {
     private long startMeasuringTimestamp;
     private Double downloadRateInMbps; // Mega bit per second
     private Double downloadRateInKBps; // Mega bit per second
-
-    public SpeedTestWebSite() {
-    }
 
     public SpeedTestWebSite(String speedTestIdentifier) {
         this.speedTestIdentifier = speedTestIdentifier;
@@ -38,77 +42,5 @@ public class SpeedTestWebSite {
         this.startMeasuringTimestamp = startMeasuringTimestamp;
     }
 
-    public String getUrlAddress() {
-        return urlAddress;
-    }
 
-    public void setUrlAddress(String urlAddress) {
-        this.urlAddress = urlAddress;
-    }
-
-    public String getSpeedTestIdentifier() {
-        return speedTestIdentifier;
-    }
-
-    public void setSpeedTestIdentifier(String speedTestIdentifier) {
-        this.speedTestIdentifier = speedTestIdentifier;
-    }
-
-    public long getStartMeasuringTimestamp() {
-        return startMeasuringTimestamp;
-    }
-
-    public void setStartMeasuringTimestamp(long startMeasuringTimestamp) {
-        this.startMeasuringTimestamp = startMeasuringTimestamp;
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public void setPath(String path) {
-        this.path = path;
-    }
-
-    public Double getDownloadRateInMbps() {
-        return downloadRateInMbps;
-    }
-
-    public void setDownloadRateInMbps(Double downloadRateInMbps) {
-        this.downloadRateInMbps = downloadRateInMbps;
-    }
-
-    public boolean isSucceed() {
-        return succeed;
-    }
-
-    public void setSucceed(boolean succeed) {
-        this.succeed = succeed;
-    }
-
-    public String getNonFlashResult() {
-        return nonFlashResult;
-    }
-
-    public void setNonFlashResult(String nonFlashResult) {
-        this.nonFlashResult = nonFlashResult;
-    }
-
-    public Double getDownloadRateInKBps() {
-        return downloadRateInKBps;
-    }
-
-    public void setDownloadRateInKBps(Double downloadRateInKBps) {
-        this.downloadRateInKBps = downloadRateInKBps;
-    }
-
-    @Override
-    public String toString() {
-        return "SpeedTestWebSite{" +
-                "succeed=" + succeed +
-                ", urlAddress='" + urlAddress + '\'' +
-                ", speedTestIdentifier='" + speedTestIdentifier + '\'' +
-                ", startMeasuringTimestamp=" + startMeasuringTimestamp +
-                '}';
-    }
 }
