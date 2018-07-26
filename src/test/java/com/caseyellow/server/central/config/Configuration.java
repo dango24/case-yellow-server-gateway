c4c7398bd83e1f7f682748cc8272527ab292f2e0
Dan <dangolan24@gmail.com>
2018-07-26 13:05:39 +0300

    fix tests issues

--- src/test/java/com/caseyellow/server/central/config/Configuration.java
+++ src/test/java/com/caseyellow/server/central/config/Configuration.java
@@ -1,11 +1,15 @@
 package com.caseyellow.server.central.config;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.caseyellow.server.central.domain.mail.EmailService;
+import com.caseyellow.server.central.persistence.statistics.repository.UserStatisticsRepository;
 import com.caseyellow.server.central.persistence.test.model.LastUserTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 
 import java.util.List;
+
+import static org.mockito.Mockito.mock;
 
 @org.springframework.context.annotation.Configuration
 public class Configuration {
@@ -13,16 +17,21 @@ public class Configuration {
     @Bean
     @Profile("dev")
     public EmailService devEmailService() {
-        return new EmailService() {
-            @Override
-            public void sendEmails(List<LastUserTest> lastUserTests) {
+        EmailService emailService = mock(EmailService.class);
+        return emailService;
+    }
 
-            }
+    @Bean
+    @Profile("dev")
+    public AmazonDynamoDB amazonDynamoDB() {
+        AmazonDynamoDB amazonDynamoDB = mock(AmazonDynamoDB.class);
+        return amazonDynamoDB;
+    }
 
-            @Override
-            public void sendImageSanity(String imageSanityPath) {
-
-            }
-        };
+    @Bean
+    @Profile("dev")
+    public UserStatisticsRepository userStatisticsRepository() {
+        UserStatisticsRepository userStatisticsRepository = mock(UserStatisticsRepository.class);
+        return userStatisticsRepository;
     }
 }
